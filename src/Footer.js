79218cce4d850ac67a160403c0ec340923e1347a
julius idowu <idowujulius92@gmail.com>
2021-07-17 17:37:01 +0100

    fix bug reformat footer file

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ export function Footer(props) {
         <footer className="mastfoot mt-auto">
             <div className="inner">
                 <p>&copy; {props.year} <a href="https://getbootstrap.com/">Portfolio</a>, by <a target="_blank"
-                    href="https://github.com/JuliRash">@JuliRash</a>.</p>
+                    href="https://github.com/JuliRash" rel="noreferrer">@JuliRash</a>.</p>
             </div>
         </footer>
     )
