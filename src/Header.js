ebaef55b370cde3fb9cdaf004161ca5259b64f3e
julius idowu <idowujulius92@gmail.com>
2021-07-17 17:36:35 +0100

    fix bug class name in header.js

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,9 @@ export function  Header() {
                 <div className="inner">
                     <h3 className="masthead-brand">JuliRash</h3>
                     <nav className="nav nav-masthead justify-content-center">
-                        <a className="nav-link active" href="#">Home</a>
-                        <a className="nav-link" href="#">Projects</a>
-                        <a className="nav-link" href="#">Contact Info</a>
+                        <a className="nav-link active" href="/">Home</a>
+                        <a className="nav-link" href="https://github.com/JuliRash">Projects</a>
+                        <a className="nav-link" href="https://linkedin.com/idowu">Contact Info</a>
                     </nav>
                 </div>
             </header>
